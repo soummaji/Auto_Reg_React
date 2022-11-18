cb504b473c3674785cb6147bde0f60ddf8f43768
shreyomu <shreyoshi.mukherjee@capgemini.com>
2022-11-18 20:14:32 +0530

    login routed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import SideNavBar from './Components/Sidebar/Sidebar';
+import Login from './Components/Login/Login';
 function App() {
   return (
     <div>
       <SideNavBar />
       <Router>
         <Routes>
+        <Route exact path='/' element={<Login/>} />
           <Route exact path='/home' element={<Home/>} />
         </Routes>
       </Router>
