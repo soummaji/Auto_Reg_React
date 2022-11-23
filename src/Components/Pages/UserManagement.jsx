b3573647c3d077f4a3fc15338dd70f918533a277
soummaji <112464303+soummaji@users.noreply.github.com>
2022-11-23 16:59:23 +0530

    4:59 || 23-11-22

--- src/Components/Pages/UserManagement.jsx
+++ src/Components/Pages/UserManagement.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react'
-
+import './UserManagement.scss'
 const Home = () => {
 
 
   return (
     <>
       <div >
-        <h4 style={{ position: "relative", top: "-10px" }}> User Management</h4>
+        <h4 id="userManagement"> User Management</h4>
 
         <div className="btn-group position-absolute top-0 end-0 mx-4 my-2 dropdown-center">
           <button className="btn bg-transparent dropdown-toggle border-0" data-bs-toggle="dropdown" >
@@ -22,27 +22,27 @@ const Home = () => {
         </div>
 
         <div className="card border-0" >
-          <div className='card-header bg-dark border border-dark' style={{ position: "relative", color: "#F3F2F2" }}>
+          <div className='card-header bg-dark border border-dark' id="user-parent-card-header">
             <div className='ms-1'>Home / User Management</div>
           </div>
-          <div class="card-body" style={{ backgroundColor: "#EAE9E9" }}>
+          <div class="card-body" id="user-parent-card-body">
             <h5>New Users</h5>
             <div className="mx-auto ms-2 me-2">
               <div className="row">
 
-                <div class="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" style={{ width: "250px", height: "200px" }}>
+                <div class="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="user-child-card">
                   <div className="card-body">
                     <h6 className="position-absolute top-0 start-0 ms-3 mt-2">Onboarding Requests <i class="bi bi-info-circle small"></i></h6>
-                    <p className="mt-2" style={{ marginLeft: "-20px" }}>Review Onboarding requests of new users</p><br /><br />
-                    <i className="bi bi-person-check-fill" style={{ fontSize: "50px", position: "relative", bottom: "40px" }}></i>
+                    <p className="mt-2">Review Onboarding requests of new users</p><br /><br />
+                    <i className="bi bi-person-check-fill" ></i>
                   </div>
                 </div>
 
-                <div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" style={{ width: "250px", height: "200px"  }}>
+                <div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="user-child-card">
                   <div className="card-body">
                     <h6 className="position-absolute top-0 start-0 mt-2 ms-3">Invite Users <i class="bi bi-info-circle small"></i></h6>
-                    <p className="mt-2" style={{ marginLeft: "-20px" }}>Onboard new users</p>
-                    <i class="bi bi-person-plus-fill" style={{ fontSize: "50px", position:"relative", top:"30px" }}></i>
+                    <p className="mt-2" >Onboard new users</p>
+                    <i class="bi bi-person-plus-fill"></i>
                   </div>
                 </div>
 
@@ -53,18 +53,18 @@ const Home = () => {
             <h5>Manage Users with existing account</h5>
             <div className="mx-auto ms-2 me-2">
               <div className="row">
-                <div class="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" style={{ width: "250px", height: "200px" }}>
+                <div class="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="user-child-card">
                   <div className="card-body">
                     <h6 className="position-absolute top-0 start-0 ms-3 mt-2">Role Requests <i class="bi bi-info-circle small"></i></h6>
-                    <p className="mt-2" style={{ marginLeft: "-20px" }}>Review Role requests of existing users</p><br /><br />
-                    <i class="bi bi-person-fill-gear" style={{ fontSize: "50px", position: "relative", bottom: "40px" }}></i>
+                    <p className="mt-2" >Review Role requests of existing users</p><br /><br />
+                    <i class="bi bi-person-fill-gear" ></i>
                   </div>
                 </div>
-                <div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" style={{ width: "250px", height: "200px" }}>
+                <div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="user-child-card" >
                   <div className="card-body">
                     <h6 className="position-absolute top-0 start-0 mt-2 ms-3">Lock-Unlock users <i class="bi bi-info-circle small"></i></h6>
-                    <p className="mt-2" style={{ marginLeft: "-20px" }}>Enable/disable login for the users to the portal</p>
-                    <i class="bi bi-person-fill-lock" style={{ fontSize: "50px" }}></i>
+                    <p className="mt-2" >Enable/disable login for the users to the portal</p>
+                    <i class="bi bi-person-fill-lock" ></i>
                   </div>
                 </div>
               </div>
