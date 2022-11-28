5c4816f2b8307c3709857f3ef13604fdaca95495
shreyomu <shreyoshi.mukherjee@capgemini.com>
2022-11-28 12:02:52 +0530

    cards added

--- src/Components/Data/application.js
+++ src/Components/Data/application.js
@@ -42,6 +42,98 @@ const application = [
                 Button: "NA",
                 Enabled:false
             },
+            {
+                id: 6,
+                Name: "BroTAP",
+                Desc: "Advice of transports to Brose plants.",
+                Access: "NA",
+                Button: "NA",
+                Enabled:false
+            },
+            {
+                id: 7,
+                Name: "Supplier Workplace(SWP)",
+                Desc: "Administration of shipping notifications to Brose plants.",
+                Access: "NA",
+                Button: "NA",
+                Enabled:false
+            },
+            {
+                id: 8,
+                Name: "Brose Overseas-Process(OCC)",
+                Desc: "Data maintenance by OCC.",
+                Access: "NA",
+                Button: "NA",
+                Enabled:false
+            },
+        ],
+        Quality: [
+            {
+                id: 1,
+                Name: "Astras",
+                Desc: "Request for quotation",
+                Access: "Green",
+                Button: "Start",
+                Enabled: true
+            },
+            {
+                id: 2,
+                Name: "Sampling (PPA / PPAP)",
+                Desc: "Production process and product approval",
+                Access: "NA",
+                Button: "NA",
+                Enabled: false
+            },
+            {
+                id:3,
+                Name: "Supplier Number",
+                Desc: "Supplier Number short description",
+                Access: "NA",
+                Button: "NA",
+                Enabled: false
+            },
+            {
+                id: 4,
+                Name: "Feasibility Study (FS)",
+                Desc: "Feasibility Study for supplier selection",
+                Access: "Red",
+                Button: "NA",
+                Enabled: false
+            },
+            {
+                id: 5,
+                Name: "Claim Processing 8D",
+                Desc: "8D processing supplier",
+                Access: "NA",
+                Button: "NA",
+                Enabled: false
+            },
+            {
+                id: 6,
+                Name: "Deviation Request AWE",
+                Desc: "Online Application Deviation Request",
+                Access: "NA",
+                Button: "NA",
+                Enabled: false
+            },
+        ],
+        Marketing: [
+            {
+                id: 1,
+                Name: "Global Customs Service (GCS)",
+                Desc: "Maintenance of customs data",
+                Access: "Yellow",
+                Button: "NA",
+                Enabled: true
+            },
+            {
+                id: 2,
+                Name: "BCN",
+                Desc: "Sharepoint cooperation platform by Brose group",
+                Access: "NA",
+                Button: "Open",
+                Enabled: false
+            },
         ]
     }
 ]
--- src/Components/Pages/Application.jsx
+++ src/Components/Pages/Application.jsx
@@ -102,6 +102,105 @@ const Application = () => {
                 })}
               </div>
             </div>
+            <h5>Quality</h5>
+            <div className="mx-auto ms-2 me-2">
+              <div className="row">
+
+                {application.map((item) => {
+                  return (
+                    <div className="row">
+                      {item.Quality.map((data) => {
+                        return (<div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="appli-child-card">
+                          <div className="card-body">
+                            <h6 className="position-absolute top-0 start-0 ms-3 mt-2">{data.Name} <i className="bi bi-info-circle small"></i></h6>
+                            <p className="mt-2">{data.Desc}</p><br /><br />
+
+                            {data.Button == "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                              <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
+                                Start
+                              </button>
+                              <ul className="dropdown-menu dropdown-menu-dark">
+                                <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
+                                <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
+                              </ul>
+                            </div>) : data.Button == "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
+                              : ("")}
+
+
+                            {data.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                              <i className="bi bi-circle-fill me-1 " id="grey" ></i>
+                              <i className="bi bi-circle-fill me-1" id="grey"></i>
+                              <i className="bi bi-circle-fill" id="green"></i></div>
+                            ) : data.Access == "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                              <i className="bi bi-circle-fill me-1 " id="red" ></i>
+                              <i className="bi bi-circle-fill me-1" id="grey"></i>
+                              <i className="bi bi-circle-fill" id="grey"></i></div>
+
+                            ) : data.Access == "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                              <i className="bi bi-circle-fill me-1 " id="grey" ></i>
+                              <i className="bi bi-circle-fill me-1" id="orange"></i>
+                              <i className="bi bi-circle-fill" id="grey"></i></div>)
+                              : ("")}
+
+                          </div>
+                        </div>)
+                      })}</div>
+                  )
+                })}
+              </div>
+            </div>
+            <h5>Marketing</h5>
+            <div className="mx-auto ms-2 me-2">
+              <div className="row">
+
+                {application.map((item) => {
+                  return (
+                    <div className="row">
+                      {item.Marketing.map((data) => {
+                        return (<div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="appli-child-card">
+                          <div className="card-body">
+                            <h6 className="position-absolute top-0 start-0 ms-3 mt-2">{data.Name} <i className="bi bi-info-circle small"></i></h6>
+                            <p className="mt-2">{data.Desc}</p><br /><br />
+
+                            {data.Button == "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                              <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
+                                Start
+                              </button>
+                              <ul className="dropdown-menu dropdown-menu-dark">
+                                <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
+                                <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
+                              </ul>
+                            </div>) : data.Button == "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
+                              : ("")}
+
+
+                            {data.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                              <i className="bi bi-circle-fill me-1 " id="grey" ></i>
+                              <i className="bi bi-circle-fill me-1" id="grey"></i>
+                              <i className="bi bi-circle-fill" id="green"></i></div>
+                            ) : data.Access == "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                              <i className="bi bi-circle-fill me-1 " id="red" ></i>
+                              <i className="bi bi-circle-fill me-1" id="grey"></i>
+                              <i className="bi bi-circle-fill" id="grey"></i></div>
+
+                            ): data.Access == "Yellow" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            <i className="bi bi-circle-fill me-1 " id="grey" ></i>
+                            <i className="bi bi-circle-fill me-1" id="yellow"></i>
+                            <i className="bi bi-circle-fill" id="grey"></i></div>
+
+                            ): data.Access == "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                              <i className="bi bi-circle-fill me-1 " id="grey" ></i>
+                              <i className="bi bi-circle-fill me-1" id="orange"></i>
+                              <i className="bi bi-circle-fill" id="grey"></i></div>)
+                              : ("")}
+
+                          </div>
+                        </div>)
+                      })}</div>
+                  )
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </div>
