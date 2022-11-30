64ed7a9b7eff0526ef0d4cc5e012c7ed871f01a2
soummaji <112464303+soummaji@users.noreply.github.com>
2022-11-30 14:38:22 +0530

    ..

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import './Home.scss'
 import home from '../Data/home.js'
+import Card from '../../Cards/Card'
 const Home = () => {
 
 
@@ -21,32 +22,33 @@ const Home = () => {
 
                 {home.map((item) => {
                   return (
-                    <div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="home-child-card">
-                      <div className="card-body">
-                        <h6 className="position-absolute top-0 start-0 ms-3 mt-2">{item.Name} <i className="bi bi-info-circle small"></i></h6>
-                        <p className="mt-2">{item.Desc}</p><br /><br />
-
-
-                        {item.Button == "start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
-                          <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
-                            Start
-                          </button>
-                          <ul className="dropdown-menu dropdown-menu-dark">
-                            <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
-                            <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
-                          </ul>
-                        </div>
-                        ) : (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)}
-
-                        {item.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
-                          <i className="bi bi-circle-fill me-1 " id="grey" ></i>
-                          <i className="bi bi-circle-fill me-1" id="grey"></i>
-                          <i className="bi bi-circle-fill" id="green"></i></div>) : (
-                          ""
-                        )}
-
-                      </div>
-                    </div>
+                        <Card Name={item.Name} Desc={item.Desc} Button={item.Button} Access={item.Access} />
+                    //     <div className="card border-0 col-md-5 me-3 my-3 shadow p-3 mb-5 bg-body rounded" id="home-child-card">
+                    //     <div className="card-body">
+                    //         <h6 className="position-absolute top-0 start-0 ms-3 mt-2">{item.Name} <i className="bi bi-info-circle small"></i></h6>
+                    //         <p className="mt-2">{item.Desc}</p><br /><br />
+    
+    
+                    //         {item.Button == "start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                    //             <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
+                    //                 Start
+                    //             </button>
+                    //             <ul className="dropdown-menu dropdown-menu-dark">
+                    //                 <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
+                    //                 <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
+                    //             </ul>
+                    //         </div>
+                    //         ) : (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)}
+    
+                    //         {item.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                    //             <i className="bi bi-circle-fill me-1 " id="grey" ></i>
+                    //             <i className="bi bi-circle-fill me-1" id="grey"></i>
+                    //             <i className="bi bi-circle-fill" id="green"></i></div>) : (
+                    //             ""
+                    //         )}
+    
+                    //     </div>
+                    // </div>
                   )
                 })}
 
--- src/Components/Pages/Application.jsx
+++ src/Components/Pages/Application.jsx
@@ -30,7 +30,7 @@ const Application = () => {
                             <h6 className="position-absolute top-0 start-0 ms-3 mt-2">{data.Name} <i className="bi bi-info-circle small"></i></h6>
                             <p className="mt-2">{data.Desc}</p><br /><br />
 
-                            {data.Button == "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                            {data.Button === "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
                               <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
                                 Start
                               </button>
@@ -38,20 +38,20 @@ const Application = () => {
                                 <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
                                 <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
                               </ul>
-                            </div>) : data.Button == "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
+                            </div>) : data.Button === "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
                               : ("")}
 
 
-                            {data.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            {data.Access === "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="green"></i></div>
-                            ) : data.Access == "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ) : data.Access === "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="red" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>
 
-                            ) : data.Access == "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ) : data.Access === "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="orange"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>)
@@ -73,11 +73,11 @@ const Application = () => {
                     <div className='row'>
                       {item.Logistics.map((data) => {
                         return (
-                          <>{data.Enabled == true ? (<div className="card border-0 col-md-3 me-3 mt-3 shadow p-3 mb-5 bg-body rounded" id="appli-child-card-2" >
+                          <>{data.Enabled === true ? (<div className="card border-0 col-md-3 me-3 mt-3 shadow p-3 mb-5 bg-body rounded" id="appli-child-card-2" >
                             <div className="card-body">
                               <h6 className="position-absolute top-0 start-0 mt-2 ms-3 ">{data.Name} <i className="bi bi-info-circle small"></i></h6>
                               <p className="mt-2">{data.Desc}</p><br /><br />
-                              {data.Button == "Open" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                              {data.Button === "Open" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
                                 <button className="btn btn-danger">
                                   Open
                                 </button>
@@ -88,7 +88,7 @@ const Application = () => {
                               <h6 className="position-absolute top-0 start-0 mt-2 ms-3 ">{data.Name} <i className="bi bi-info-circle small"></i></h6>
                               <p className="mt-2">{data.Desc}</p><br /><br />
                               <div className='position-absolute bottom-0 start-0 mb-3 ms-3' style={{fontSize:"13px", color:"gray"}}><i class="bi bi-file-earmark-lock2" style={{fontSize:"30px"}}></i> Not enabled for this supplier</div>
-                              {data.Button == "Open" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                              {data.Button === "Open" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
                                 <button className="btn btn-danger">
                                   Open
                                 </button>
@@ -111,11 +111,11 @@ const Application = () => {
                     <div className='row'>
                       {item.Quality.map((data) => {
                         return (
-                          <>{data.Enabled == true ? (<div className="card border-0 col-md-3 me-3 mt-3 shadow p-3 mb-5 bg-body rounded" id="appli-child-card-2" >
+                          <>{data.Enabled === true ? (<div className="card border-0 col-md-3 me-3 mt-3 shadow p-3 mb-5 bg-body rounded" id="appli-child-card-2" >
                             <div className="card-body">
                               <h6 className="position-absolute top-0 start-0 mt-2 ms-3 ">{data.Name} <i className="bi bi-info-circle small"></i></h6>
                               <p className="mt-2">{data.Desc}</p><br /><br />
-                              {data.Button == "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                              {data.Button === "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
                               <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
                                  Start
                                 </button>
@@ -123,18 +123,18 @@ const Application = () => {
                                 <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
                                 <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
                               </ul>
-                              </div>) : data.Button == "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
+                              </div>) : data.Button === "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
                               : ("")}
-                              {data.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                              {data.Access === "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="green"></i></div>
-                            ) : data.Access == "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ) : data.Access === "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="red" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>
 
-                            ) : data.Access == "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ) : data.Access === "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="orange"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>)
@@ -145,7 +145,7 @@ const Application = () => {
                               <h6 className="position-absolute top-0 start-0 mt-2 ms-3 ">{data.Name} <i className="bi bi-info-circle small"></i></h6>
                               <p className="mt-2">{data.Desc}</p><br /><br />
                               <div className='position-absolute bottom-0 start-0 mb-3 ms-3' style={{fontSize:"13px", color:"gray"}}><i class="bi bi-file-earmark-lock2" style={{fontSize:"30px"}}></i> Not enabled for this supplier</div>
-                              {data.Button == "Open" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                              {data.Button === "Open" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
                                 <button className="btn btn-danger">
                                   Start
                                 </button>
@@ -173,7 +173,7 @@ const Application = () => {
                             <h6 className="position-absolute top-0 start-0 ms-3 mt-2">{data.Name} <i className="bi bi-info-circle small"></i></h6>
                             <p className="mt-2">{data.Desc}</p><br /><br />
 
-                            {data.Button == "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                            {data.Button === "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
                               <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
                                 Start
                               </button>
@@ -181,25 +181,25 @@ const Application = () => {
                                 <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
                                 <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
                               </ul>
-                            </div>) : data.Button == "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
+                            </div>) : data.Button === "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
                               : ("")}
 
 
-                            {data.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            {data.Access === "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="green"></i></div>
-                            ) : data.Access == "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ) : data.Access === "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="red" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>
 
-                            ): data.Access == "Yellow" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ): data.Access === "Yellow" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                             <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                             <i className="bi bi-circle-fill me-1" id="yellow"></i>
                             <i className="bi bi-circle-fill" id="grey"></i></div>
 
-                            ): data.Access == "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ): data.Access === "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="orange"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>)
@@ -225,7 +225,7 @@ const Application = () => {
                             <h6 className="position-absolute top-0 start-0 ms-3 mt-2">{data.Name} <i className="bi bi-info-circle small"></i></h6>
                             <p className="mt-2">{data.Desc}</p><br /><br />
 
-                            {data.Button == "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
+                            {data.Button === "Start" ? (<div className="btn-group position-absolute bottom-0 end-0 mx-2 my-2">
                               <button className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
                                 Start
                               </button>
@@ -233,19 +233,19 @@ const Application = () => {
                                 <li><a className="dropdown-item" href="#">Astras app link 1 </a></li>
                                 <li><a className="dropdown-item" href="#">Astras app link 2 </a></li>
                               </ul>
-                            </div>) : data.Button == "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
+                            </div>) : data.Button === "Open" ? (<button className='btn btn-danger position-absolute bottom-0 end-0 mx-2 my-2'>Open</button>)
                               : ("")}
 
-                            {data.Access == "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            {data.Access === "Green" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="green"></i></div>
-                            ) : data.Access == "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ) : data.Access === "Red" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="red" ></i>
                               <i className="bi bi-circle-fill me-1" id="grey"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>
 
-                            ) : data.Access == "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
+                            ) : data.Access === "Orange" ? (<div className="position-absolute bottom-0 start-0  mb-3 ms-3" >
                               <i className="bi bi-circle-fill me-1 " id="grey" ></i>
                               <i className="bi bi-circle-fill me-1" id="orange"></i>
                               <i className="bi bi-circle-fill" id="grey"></i></div>)
